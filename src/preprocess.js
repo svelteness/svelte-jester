8783131a538b6c5263f2069b00d252f44d7acc0e
Rahim Alwer <rahim_alwer@hotmail.com>
2019-12-06 09:40:47 +1100

    fix: cosmiconfig was loading config from pkg if svelte key was defined

--- src/preprocess.js
+++ src/preprocess.js
@@ -1,9 +1,12 @@
+const path = require('path')
 const svelte = require('svelte/compiler')
 const { cosmiconfigSync } = require('cosmiconfig')
 
 function preprocess () {
   return ({ source, filename }) => {
-    const config = cosmiconfigSync('svelte').search().config
+    // TODO: Can svelte.config.js be anywhere else?
+    const configPath = path.join(process.cwd(), 'svelte.config.js')
+    const config = cosmiconfigSync().load(configPath).config
     return svelte
       .preprocess(source, config.preprocess || {}, { filename })
       .then(r => r.code)
