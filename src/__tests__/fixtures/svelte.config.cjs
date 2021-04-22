662261127e4c0e26f280857842266c326897e72b
Sebastian Rothe <rothe@infai.org>
2021-04-22 13:07:26 +0200

    add test to check passing of env variables
    NODE_ENV in this test

--- src/__tests__/fixtures/svelte.config.cjs
+++ src/__tests__/fixtures/svelte.config.cjs
@@ -2,6 +2,10 @@ const { replace } = require('svelte-preprocess')
 
 module.exports = {
   preprocess: [
-    replace([[/Hello/gi, 'Bye']])
+    replace([
+      [/Hello/gi, 'Bye'],
+      // replace env var
+      [/process\.env\.NODE_ENV/gi, JSON.stringify(process.env.NODE_ENV)]
+    ])
   ]
 }
