1d9bebd67b7123492c7335ecde86e730edf6ba38
Sebastian Rothe <rothe@infai.org>
2021-05-20 23:49:19 +0200

    refactor top-level await to promise

--- src/preprocess.js
+++ src/preprocess.js
@@ -1,11 +1,11 @@
 import { preprocess } from 'svelte/compiler'
 
 const { source, filename, svelteConfig } = process.env
-const configImport = await import(svelteConfig)
+import(svelteConfig).then(configImport => {
+  // ESM or CommonJS
+  const config = configImport.default ? configImport.default : configImport
 
-// ESM or CommonJS
-const config = configImport.default ? configImport.default : configImport
-
-preprocess(source, config.preprocess || {}, { filename }).then((r) => {
-  process.stdout.write(JSON.stringify(r))
-})
+  preprocess(source, config.preprocess || {}, { filename }).then((r) => {
+    process.stdout.write(JSON.stringify(r))
+  })
+}).catch(err => process.stderr.write(err))
