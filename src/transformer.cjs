7c5ba209f402d703cae6ab42416abf7ab589dcba
Sebastian Rothe <rothe@infai.org>
2021-05-20 23:26:41 +0200

    refactor shared variables into compiler function

--- src/transformer.cjs
+++ src/transformer.cjs
@@ -2,50 +2,53 @@
 const { basename } = require('path')
 const { execSync } = require('child_process')
 const svelte = require('svelte/compiler')
-const { getSvelteConfig } = require('./svelteconfig.cjs')
+const { getSvelteConfig } = require('./svelteconfig')
 
 const transformer = (options = {}) => (source, filename) => {
-  const { debug, compilerOptions, preprocess, rootMode, maxBuffer } = options
+  const {preprocess, rootMode, maxBuffer } = options
 
-  let processedMap
-  let processedCode = source
+  if (!preprocess) {
+    return compiler(options, filename, source)
+  }
 
-  if (preprocess) {
     const svelteConfig = getSvelteConfig(rootMode, filename, preprocess)
-    const preprocessor = require.resolve('./preprocess.cjs')
+    const preprocessor = require.resolve('./preprocess.js')
+
     const preprocessResult = execSync(
-      `node --unhandled-rejections=strict --abort-on-uncaught-exception "${preprocessor}"`,
-      {
-        env: { ...process.env, source, filename, svelteConfig },
-        maxBuffer: maxBuffer || 10 * 1024 * 1024
-      }
+        `node --unhandled-rejections=strict --abort-on-uncaught-exception ${preprocessor}`,
+        {
+          env: { ...process.env, source, filename, svelteConfig },
+          maxBuffer: maxBuffer || 10 * 1024 * 1024
+        }
     ).toString()
+
     const parsedPreprocessResult = JSON.parse(preprocessResult)
-    processedCode = parsedPreprocessResult.code
-    processedMap = parsedPreprocessResult.map
-  }
+    return compiler(options, filename, parsedPreprocessResult.code, parsedPreprocessResult.map);
+}
 
-  const result = svelte.compile(processedCode, {
-    filename: basename(filename),
-    css: true,
-    accessors: true,
-    dev: true,
-    format: 'cjs',
-    sourcemap: processedMap,
-    ...compilerOptions
-  })
+const compiler = (options = {}, filename, processedCode, processedMap) => {
+  const { debug, compilerOptions} = options
 
-  if (debug) {
-    console.log(result.js.code)
-  }
+    const result = svelte.compile(processedCode, {
+        filename: basename(filename),
+        css: true,
+        accessors: true,
+        dev: true,
+        format: 'cjs',
+        sourcemap: processedMap,
+        ...compilerOptions
+    })
 
-  const esInterop =
-    'Object.defineProperty(exports, "__esModule", { value: true });'
+    if (debug) {
+        console.log(result.js.code)
+    }
 
-  return {
-    code: result.js.code + esInterop,
-    map: JSON.stringify(result.js.map)
-  }
+    const esInterop = 'Object.defineProperty(exports, "__esModule", { value: true });'
+
+    return {
+        code: result.js.code + esInterop,
+        map: JSON.stringify(result.js.map)
+    }
 }
 
 exports.createTransformer = (options) => ({
