38c735fe5d74bdda8a7cbeba0baf6aaccdfc99c6
Sebastian Rothe <rothe@infai.org>
2022-01-30 13:24:36 +0100

    rename runTransformer function

--- src/__tests__/transformer.test.js
+++ src/__tests__/transformer.test.js
@@ -8,7 +8,7 @@ import transformer from '../../dist/transformer.mjs'
 // Node API __dirname is missing in ESM
 export const __dirname = dirname(fileURLToPath(import.meta.url))
 
-const runTransformer = async (filename, options) => {
+const runTransformerAsync = async (filename, options) => {
   const path = `${__dirname}/fixtures/${filename}.svelte`
   const source = readFileSync(path).toString()
   const result = await transformer.processAsync(source, path, { transformerConfig: options })
@@ -21,7 +21,7 @@ const runTransformer = async (filename, options) => {
 describe('ESM transformer', () => {
   it('should transform with config in ESM format', async () => {
     const svelteKitConfigPath = `${__dirname}/fixtures/sveltekit.config.js`
-    const results = await runTransformer('BasicComp', {
+    const results = await runTransformerAsync('BasicComp', {
       preprocess: svelteKitConfigPath
     })
     // this is a little brittle, but it demonstrates that the replacements in
@@ -30,20 +30,20 @@ describe('ESM transformer', () => {
   })
 
   it('should transform basic component', async () => {
-    await runTransformer('BasicComp')
+    await runTransformerAsync('BasicComp')
   })
 
   it('should transform when using sass preprocessor', async () => {
-    await runTransformer('SassComp', { preprocess: true })
+    await runTransformerAsync('SassComp', { preprocess: true })
   })
 
   it('should transform when using full path to preprocess', async () => {
     const preprocessPath = `${__dirname}/../../_svelte.config.cjs`
-    await runTransformer('SassComp', { preprocess: preprocessPath })
+    await runTransformerAsync('SassComp', { preprocess: preprocessPath })
   })
 
   it('should search for "svelte.config.cjs" as well as "svelte.config.js"', async () => {
-    const results = await runTransformer('BasicComp', {
+    const results = await runTransformerAsync('BasicComp', {
       preprocess: true,
       rootMode: 'upward'
     })
@@ -53,17 +53,17 @@ describe('ESM transformer', () => {
   })
 
   it('should transform when using typescript preprocessor', async () => {
-    await runTransformer('TypescriptComp', { preprocess: true })
+    await runTransformerAsync('TypescriptComp', { preprocess: true })
   })
 
   it('should transform basic component and keep styles', async () => {
-    const code = await runTransformer('BasicComp')
+    const code = await runTransformerAsync('BasicComp')
     expect(code).toContain('add_css(target)')
     expect(code).toContain('.counter.active')
   })
 
   it('should accept compiler options', async () => {
-    const code = await runTransformer('BasicComp', {
+    const code = await runTransformerAsync('BasicComp', {
       compilerOptions: { css: false }
     })
     expect(code).not.toContain('add_css(target)')
@@ -72,7 +72,7 @@ describe('ESM transformer', () => {
 
   it('should output code to console when debug is true', async () => {
     console.log = jest.fn()
-    const code = await runTransformer('BasicComp', { debug: true })
+    const code = await runTransformerAsync('BasicComp', { debug: true })
     const esInterop =
       'Object.defineProperty(exports, "__esModule", { value: true });'
     expect(console.log).toHaveBeenCalledTimes(1)
@@ -80,16 +80,16 @@ describe('ESM transformer', () => {
   })
 
   it('should pass, if console.logs are disabled (default) during preprocessing and there is a console.log statement in the svelte config', async () => {
-    await runTransformer('BasicComp', { preprocess: true, rootMode: 'upward' })
+    await runTransformerAsync('BasicComp', { preprocess: true, rootMode: 'upward' })
   })
 
   it('should pass, if console.logs are disabled during preprocessing and there is a console.log statement in the svelte config', async () => {
-    await runTransformer('BasicComp', {
+    await runTransformerAsync('BasicComp', {
       preprocess: true,
       rootMode: 'upward',
       showConsoleLog: false
     })
-    await runTransformer('BasicComp', {
+    await runTransformerAsync('BasicComp', {
       preprocess: true,
       rootMode: 'upward',
       showConsoleLog: 'false'
@@ -97,7 +97,7 @@ describe('ESM transformer', () => {
   })
 
   it('should pass and transform process.env.NODE_ENV variable', async () => {
-    const code = await runTransformer('BasicComp', {
+    const code = await runTransformerAsync('BasicComp', {
       preprocess: true,
       rootMode: 'upward'
     })
