31707f889f87362c2fdea201831a952973eff49a
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-07-19 11:30:10 -0700

    fix sveltekit test

--- e2e/sveltekit/src/routes/index-dom.spec.ts
+++ e2e/sveltekit/src/routes/index-dom.spec.ts
@@ -1,7 +1,8 @@
 /**
  * @jest-environment jsdom
  */
-import { render, RenderResult } from '@testing-library/svelte';
+
+import { render } from '@testing-library/svelte';
 import Index from './index.svelte';
 
 /**
@@ -14,15 +15,9 @@ import Index from './index.svelte';
 
 describe('Index', () => {
 
-  let renderedComponent: RenderResult;
-
-  beforeEach(() => {
-    renderedComponent = render(Index);
-  });
-
   describe('once the component has been rendered', () => {
     test('should show the proper heading', () => {
-      expect(renderedComponent.getByText((content, element) => content.startsWith('to your new'))).toBeInTheDocument();
+      expect(render(Index).getByText((content, element) => content.startsWith('to your new'))).toBeInTheDocument();
     });
 
   });
