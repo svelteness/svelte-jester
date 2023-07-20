45399c3db4699a2a2933bc6716265d61b54bf40e
Sebastian Rothe <rothe@infai.org>
2023-07-20 15:33:31 +0200

    fix: lock testing-library/svelte to 3.x fix: add missing jest-environment-jsdom
    testing-library/svelte@4 requires ESM

--- e2e/init-svelte.sh
+++ e2e/init-svelte.sh
@@ -5,6 +5,6 @@ cd svelte
 npx degit sveltejs/template . --force
 
 npm install
-npm install --save-dev svelte-preprocess jest svelte-jester @babel/preset-env babel-jest @testing-library/svelte @testing-library/jest-dom
+npm install --save-dev svelte-preprocess jest jest-environment-jsdom svelte-jester @babel/preset-env babel-jest @testing-library/svelte@3 @testing-library/jest-dom
 
 cd ..
