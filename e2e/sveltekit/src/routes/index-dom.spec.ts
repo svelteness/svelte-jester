a70c6f751b7d242efa41f833655122d81778d71f
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-07-22 19:47:25 -0700

    fix test

--- e2e/sveltekit/src/routes/index-dom.spec.ts
+++ e2e/sveltekit/src/routes/index-dom.spec.ts
@@ -1,30 +1,21 @@
 /**
  * @jest-environment jsdom
  */
-import { jest } from '@jest/globals'
-import { render, RenderResult } from '@testing-library/svelte';
-import Index from './Counter.svelte';
+
+import { render } from '@testing-library/svelte';
+import Counter from './Counter.svelte';
 
 /**
- * An example test suite outlining the usage of
- * `describe()`, `beforeEach()`, `test()` and `expect()`
- *
  * @see https://jestjs.io/docs/getting-started
  * @see https://github.com/testing-library/jest-dom
  */
 
 describe('Index', () => {
 
-  let renderedComponent: RenderResult<Index>;
-
-  beforeEach(() => {
-    renderedComponent = render(Index);
-  });
-
   describe('once the component has been rendered', () => {
 
     test('should show the proper label', () => {
-      expect(renderedComponent.getByLabelText(/Decrease the counter by one/)).toBeInTheDocument();
+      expect(render(Counter).getByLabelText(/Decrease the counter by one/)).toBeInTheDocument();
     });
 
   });
