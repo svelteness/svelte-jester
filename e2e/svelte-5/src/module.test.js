f9a947d18255ef4cbe36239e936f705908d32cc2
Sebastian Rothe <rothe@infai.org>
2024-06-11 00:39:08 +0200

    feat: enable import

--- e2e/svelte-5/src/module.test.js
+++ e2e/svelte-5/src/module.test.js
@@ -1,9 +1,6 @@
 import { test } from '@jest/globals'
 
-// TODO(mcous, 2024-05-23): this import fails
-// See https://github.com/svelteness/svelte-jester/pull/283
-// import * as Subject from "./module.svelte.js";
-const Subject = {}
+import * as Subject from './module.svelte.js'
 
 test.skip('get current count', () => {
   const subject = Subject.createCounter()
