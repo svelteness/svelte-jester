8356e0fe6b254919e52c287ecb7d8900f89d11fb
Sebastian Rothe <rothe@infai.org>
2024-06-11 00:39:17 +0200

    fix: enable tests

--- e2e/svelte-5/src/module.test.js
+++ e2e/svelte-5/src/module.test.js
@@ -2,14 +2,14 @@ import { test } from '@jest/globals'
 
 import * as Subject from './module.svelte.js'
 
-test.skip('get current count', () => {
+test('get current count', () => {
   const subject = Subject.createCounter()
   const result = subject.count
 
   expect(result).toBe(0)
 })
 
-test.skip('increment', () => {
+test('increment', () => {
   const subject = Subject.createCounter()
 
   subject.increment()
