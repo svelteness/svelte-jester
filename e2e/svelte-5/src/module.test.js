2f89077f2748a499714f01e2d797df17b4f6419e
Sebastian Rothe <rothe@infai.org>
2024-06-11 00:27:44 +0200

    fix: pass linter

--- e2e/svelte-5/src/module.test.js
+++ e2e/svelte-5/src/module.test.js
@@ -3,6 +3,7 @@ import { test } from '@jest/globals'
 // TODO(mcous, 2024-05-23): this import fails
 // See https://github.com/svelteness/svelte-jester/pull/283
 // import * as Subject from "./module.svelte.js";
+const Subject = {}
 
 test.skip('get current count', () => {
   const subject = Subject.createCounter()
