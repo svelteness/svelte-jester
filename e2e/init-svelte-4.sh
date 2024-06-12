d9f4f2cc0977f893d0ec0775b6628158c48325a6
Sebastian Rothe <rothe@infai.org>
2024-06-12 22:50:24 +0200

    feat: add svelte4 test project

---
+++ e2e/init-svelte-4.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+npm init vite
+# select svelte, js and name "svelte-4"
+cd svelte-4
+
+npm install
+# apply all steps from https://github.com/rossyman/svelte-add-jest/blob/main/preset.ts
+
+cd ..
