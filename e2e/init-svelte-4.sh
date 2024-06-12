08f282150c654cc4f099f421cf161e4fbd4245ce
Sebastian Rothe <rothe@infai.org>
2024-06-12 22:59:57 +0200

    feat: add test dependencies

--- e2e/init-svelte-4.sh
+++ e2e/init-svelte-4.sh
@@ -5,6 +5,9 @@ npm init vite
 cd svelte-4
 
 npm install
+
+# install test dependencies
+npm install --save-dev svelte-preprocess jest jest-environment-jsdom svelte-jester @babel/preset-env babel-jest @testing-library/svelte@4 @testing-library/jest-dom
 # apply all steps from https://github.com/rossyman/svelte-add-jest/blob/main/preset.ts
 
 cd ..
