71076e559d18e5ee2a8fd1c7e55cf263d4cea06d
Sebastian Rothe <rothe@infai.org>
2024-06-15 00:18:07 +0200

    feat: add test dependencies, setup and scripts

--- e2e/init-svelte-4.sh
+++ e2e/init-svelte-4.sh
@@ -4,10 +4,14 @@ npm init vite
 # select svelte, js and name "svelte-4"
 cd svelte-4
 
-npm install
+pnpm install
 
 # install test dependencies
-npm install --save-dev svelte-preprocess jest jest-environment-jsdom svelte-jester @babel/preset-env babel-jest @testing-library/svelte@4 @testing-library/jest-dom
+pnpm install --save-dev svelte-preprocess jest jest-environment-jsdom svelte-jester@workspace @testing-library/svelte@4 @testing-library/jest-dom
 # apply all steps from https://github.com/rossyman/svelte-add-jest/blob/main/preset.ts
+
+# add svg transformer
+pnpm i -D jest-svg-transformer@https://github.com/Em-Ant/jest-transformer-svg
+# add transform config to package.json `"^.+\\.svg$": "jest-svg-transformer"`
 
 cd ..
